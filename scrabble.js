a00f46833f021553fc95efa8329f8943144167b1
Danielle Schrimmer <danielle.schrimmer@gmail.com>
2016-11-15 14:00:08 -0800

    Add function names

--- scrabble.js
+++ scrabble.js
@@ -1,5 +1,14 @@
 var Scrabble = function() {};
 
+Scrabble.prototype.score = function(word) {
+
+};
+
+Scrabble.prototype.highestScoreFrom = function(arrayOfWords) {
+  
+};
+
+
 // YOUR CODE HERE
 Scrabble.prototype.helloWorld = function() {
   return 'hello world!';
