693f8062b31c85314aca6c16d31c536e426a57e4
Danielle Schrimmer <danielle.schrimmer@gmail.com>
2016-11-15 14:10:05 -0800

    Add arrays of letters for scores

--- scrabble.js
+++ scrabble.js
@@ -1,11 +1,19 @@
-var Scrabble = function() {};
+var Scrabble = function() {
+  this._onePoint = ['a','e','i','o','u','l','n','r','s','t'];
+  this._twoPoints = ['d','g'];
+  this._threePoints = ['b','c','m','p'];
+  this._fourPoints = ['f','h','v','w','y'];
+  this._fivePoints = ['k'];
+  this._eightPoints = ['j','x'];
+  this._tenPoints = ['q','z'];
+};
 
 Scrabble.prototype.score = function(word) {
-
+  
 };
 
 Scrabble.prototype.highestScoreFrom = function(arrayOfWords) {
-  
+
 };
 
 
