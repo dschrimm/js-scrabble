20aaf2ea60f1a5bdc99847d359ddd91229ea8d6c
Danielle Schrimmer <danielle.schrimmer@gmail.com>
2016-11-16 15:08:10 -0800

    highest score fn gets score of each word

--- scrabble.js
+++ scrabble.js
@@ -5,10 +5,9 @@ var Scrabble = function() {
 Scrabble.prototype.score = function(string) {
   var word = string.toLowerCase();
   var wordScore = 0;
-
-  for (i = 0; i < word.length; i++) {
+  for (k = 0; k < word.length; k++) {
     for (j = 0; j < this._letterScores.length; j++) {
-      if (this._letterScores[j].includes(word[i])) {
+      if (this._letterScores[j].includes(word[k])) {
         wordScore += (j + 1);
         break;
       }
@@ -21,12 +20,25 @@ Scrabble.prototype.score = function(string) {
 };
 
 Scrabble.prototype.highestScoreFrom = function(arrayOfWords) {
+  var scores = [];
+  for (i = 0; i < arrayOfWords.length; i++) {
+    var played_word = arrayOfWords[i];
+    var word_score = this.score(arrayOfWords[i]);
 
+    var obj = {
+      word: played_word,
+      score: word_score
+    };
+
+    scores.push(obj);
+  }
+  // return Math.max(scores);
+  return scores;
 };
 
 var s = new Scrabble();
 
-console.log(s.score("HeLlOsd"));
+console.log(s.highestScoreFrom(["HeLlOs", "hi", "asdfjkl"]));
 
 
 
